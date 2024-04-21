81d4f193bb111007921ef607f8cb7549f3b1734c
CStalks <swiafe.dev@gmail.com>
2024-04-21 18:05:20 -0400

    Solve beautiful matrix

---
+++ 800/beautifulmatrix.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+   int n,row,col;
+
+   for(int i = 1; i <= 5; i++){
+      for(int j = 1; j <= 5; j++){
+         cin >> n;
+         if(n == 1){
+            row = i;
+            col = j;
+            break;
+         }
+      }
+   }
+   cout <<abs(3-row)+abs(3-col)<< '\n';
+}
