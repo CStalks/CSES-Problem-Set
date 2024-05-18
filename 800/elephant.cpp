846760561272f47df591f820294f4e8827e180f2
CStalks <swiafe.dev@gmail.com>
2024-05-18 10:14:27 -0400

    Fix elephant

--- 800/elephant.cpp
+++ 800/elephant.cpp
@@ -4,27 +4,21 @@ using namespace std;
 
 int main(){
     int x; cin >> x;
-    int arr[5] = {1,2,3,4,5};
     int count = 0;
 
     while(x){
         if(x >= 5){
-            count++;
             x -= 5;
         } else if(x >= 4){
-            count++;
             x -= 4;
         } else if(x >= 3){
-            count++;
             x -= 3;
         } else if(x >= 2){
-            count++;
             x -= 2;
         } else if(x >= 1){
-            count++;
             x -= 1;
         }
-
+        count++;
     }
     cout << count << '\n';
 }
